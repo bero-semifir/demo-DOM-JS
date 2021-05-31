683cca253b0b9f038aaf2dde00861f908216e73d
Thomas.D <d.thomas@semifir.com>
2021-05-31 14:15:55 +0200

    Ajout de la modif CSS en inline

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,23 @@
+// Récup d'élements
 const bonGout = document.querySelector("p.bonGout");
 
 const emailInput = document.querySelector("#email");
 
 const alerteMail = document.getElementById("alerteMail");
 
+// Écoute de l'événement "input" sur <input/>
 emailInput.addEventListener("input", verifMail);
 
+// la fonction qui vérifie que l'email correspond à la valeur indiquée
 function verifMail(){
     console.log(emailInput.value);
+    // Modification du DOM
     if(emailInput.value !== "poulet@gmail.com"){
         document.querySelector("button").disabled = true;
-        alerteMail.classList.remove("hidden")
+        alerteMail.classList.remove("hidden");
     }else{
         document.querySelector("button").disabled = false;
-        alerteMail.classList.add("hidden")
+        alerteMail.classList.add("hidden");
     }
 }
 
@@ -21,4 +25,6 @@ function verifMail(){
 // il déclenche la fonction à chaques clics
 bonGout.addEventListener("click", (event)=>{
     console.log(event);
+    console.log(bonGout);
+    bonGout.style.color = "red";
 })
